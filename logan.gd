387694243d193066931aecda932ed056422347d0
Loganadams56 <143815310+Loganadams56@users.noreply.github.com>
2023-09-15 12:25:01 -0500

    update 9/15
    update

--- logan.gd
+++ logan.gd
@@ -1,19 +1,18 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
-func _on_button_pressed():
-	$labe.text ="logan adams"
 	
-	
-	func _on_btn_clear_pressed():
-	$label.text = ""
+
+
+
+func _on_btnclear_pressed():
+	$Label.text = ""
+
+
+func _on_btnclick_pressed():
+	$Label.text = "My name is logan"
+
