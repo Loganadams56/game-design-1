982f4c350406c7db19f121dc7770fa231a4fa1c9
Loganadams56 <143815310+Loganadams56@users.noreply.github.com>
2023-09-19 12:51:50 -0500

    Update prog88a.gd
    up

--- prog88a.gd
+++ prog88a.gd
@@ -12,13 +12,38 @@ func _process(delta):
 
 
 func _on_btncalc_pressed():
-	pass # Replace with function body.
+	var num1 = int($LineEdit.text)
+	var num2 = int($LineEdit2.text)
+	var Sum = num1 + num2
+	var Diff = num1 - num2
+	# Do product and average yourself
+	var Abs = abs(Diff)
+	var Max = 0
+	var Min = 0
+	if num1 > num2:
+		Max = num1
+		# Min = num2
+	else:  # Otherwise...
+		Max = num2
+	
+	if Max == num1:
+		Min = num2
+	else:
+		Min = num1
+	
+	$Label3.text = "Sum = " + str(Sum) + \
+				   "\nDifference = " + str(Diff) + \
+				
+				   "\nAbs. Distance = " + str(Abs) + \
+				   "\nMax = " + str(Max) + \
+				   "\nMin = " + str(Min)
 
 
 func _on_btnclear_pressed():
-	$label1.text = ""
-	$label2.text = ""
+	$Label.text = ""
+	$Label2.text = ""
+	$Label3.text = ""
 
 
 func _on_btnexit_pressed():
-	pass # Replace with function body.
+	get_tree().quit()
