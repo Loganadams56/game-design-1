c5589c85295f162498ee4b025d25cec329e74a29
Loganadams56 <143815310+Loganadams56@users.noreply.github.com>
2023-09-28 13:19:58 -0500

    update
    iedhf;kjlhg

--- prog_58b.gd
+++ prog_58b.gd
@@ -12,21 +12,25 @@ func _process(delta):
 
 
 
+
+
 func _on_btncalc_pressed():
 	var A = int($txtA.text)
 	var B = int($txtB.text)
-	var c = int($txtC.text)
-	var proot = (-B + sqrt(B**2 - 4 * A * C) / 2 * A
-	var nroot = (-B - sqrt(B**2 - 4 * A * c)) / 2 * A
+	var C = int($txtC.text)
+	var proot = (-B + sqrt(B**2 - 4 * A * C)) / 2 * A
+	var nroot = (-B - sqrt(B**2 - 4 * A * C)) / 2 * A
 	$lblout.text = "roots are %d and %d" % [proot, nroot]
 
 
+
+
 func _on_btnclear_pressed():
-	$txtA.text = "clear"
-	$txtB.text = "clear"
-	$txtC.text = "clear"
+	$txtA.text = ""
+	$txtB.text = ""
+	$txtC.text = ""
 	$lblout.text = ""
 
 
-func _on_btnexit_pressed():
+func _on_btnexit_2_pressed():
 	get_tree().quit()
