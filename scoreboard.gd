6af64dbb057294db2d84259ae2d50ee78dae7b3b
Loganadams56 <143815310+Loganadams56@users.noreply.github.com>
2023-10-03 12:30:00 -0500

    update
    update

---
+++ scoreboard.gd
@@ -0,0 +1,25 @@
+extends Control
+
+var score = 0
+
+func addpoint(): score += 1
+func subpiont(): score -= 1
+func reset(): score = 0
+
+func update():
+	$lblout.text = "score: %d" % score
+	
+	
+func _on_btnupdate_pressed():
+	var choice = $LineEdit.text.to_lower()
+	if choice == "add":
+		addpoint()
+	elif choice == "sub": 
+		subpiont()
+	else:
+		OS.alert("invaild choice")
+	update()
+
+func _on_btnreset_pressed():
+	reset()
+	update()
