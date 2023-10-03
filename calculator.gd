f46639e4fe30a45769ad5558c58007d23a1c5615
Loganadams56 <143815310+Loganadams56@users.noreply.github.com>
2023-10-03 12:48:49 -0500

    update
    update

---
+++ calculator.gd
@@ -0,0 +1,30 @@
+extends Control
+
+
+
+
+
+
+
+func _on_add_pressed():
+	pass # Replace with function body.
+
+
+func _on_sub_pressed():
+	pass # Replace with function body.
+
+
+func _on_multi_pressed():
+	pass # Replace with function body.
+
+
+func _on_divied_pressed():
+	pass # Replace with function body.
+
+
+func _on_clear_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	pass # Replace with function body.
