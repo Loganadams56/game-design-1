f08469da7af405426429c664b6db1e5a6254a939
Loganadams56 <143815310+Loganadams56@users.noreply.github.com>
2023-10-05 11:10:05 -0500

    update
    update

--- calculator.gd
+++ calculator.gd
@@ -2,27 +2,25 @@ extends Control
 
 
 
+var num1 = 0
+var num2 = 0
 
+func getnums():
+	num = int($LineEdit.text)
+	num = int($LineEdit.text)
 
-
-
-func _on_add_pressed():
-	myadd(num1,num2)
-
-
-func _on_sub_pressed():
-	mysub(num1,num2)
-
-
-
-func _on_multi_pressed():
-	myulti(num1,num2)
-
-
-
-func _on_divied_pressed():
-	myduvied(num1,num2)
-
+func myadd(num1, num2):
+	getNums()
+	return num1 + num2
+func mysub(num1, num2)
+	getNums()
+	return num1 - num2 
+func mymulti():
+	getNums()
+	return num1 * num2
+func mydivied():
+	get_nums()
+	return num1 % num2 
 
 func _on_clear_pressed():
 	$num1.text = ""
