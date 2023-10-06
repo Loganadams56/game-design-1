cd9ed9cfed7e900799ce97525554b428fe6d9018
Loganadams56 <143815310+Loganadams56@users.noreply.github.com>
2023-10-06 10:46:46 -0500

    update
    update

--- calculator.gd
+++ calculator.gd
@@ -5,22 +5,6 @@ extends Control
 var num1 = 0
 var num2 = 0
 
-func getnums():
-	num = int($LineEdit.text)
-	num = int($LineEdit.text)
-
-func myadd(num1, num2):
-	getNums()
-	return num1 + num2
-func mysub(num1, num2)
-	getNums()
-	return num1 - num2 
-func mymulti():
-	getNums()
-	return num1 * num2
-func mydivied():
-	get_nums()
-	return num1 % num2 
 
 func _on_clear_pressed():
 	$num1.text = ""
@@ -30,3 +14,19 @@ func _on_clear_pressed():
 
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+func _on_add_pressed():
+	pass # Replace with function body.
+
+
+func _on_sub_pressed():
+	pass # Replace with function body.
+
+
+func _on_multi_pressed():
+	pass # Replace with function body.
+
+
+func _on_divied_pressed():
+	pass # Replace with function body.
